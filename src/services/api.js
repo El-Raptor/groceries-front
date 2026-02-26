ca95d2325c733b63707a10b8fcfb08f3e2ddfe89
Felipe Salles Lopes <felipe28101997@gmail.com>
2026-02-26 16:54:59 -0400

    chore(api): add constant for Dev and Prod port

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const DEV_ENV_PORT = 8080;
+const PROD_ENV_PORT = 8090;
+
 // Criamos uma instância do axios com o endereço base do seu back-end
 export const api = axios.create({
-  baseURL: 'http://localhost:8090/api',
+  baseURL: `http://localhost:${DEV_ENV_PORT}/api`,
 });
