a48dff56f978c5359fa46628e5eea63a28a8628b
Felipe Salles Lopes <felipe28101997@gmail.com>
2026-02-26 17:01:02 -0400

    create brands listing and register page with routes

---
+++ src/services/BrandService.js
@@ -0,0 +1,11 @@
+import { api } from './api';
+
+export const getBrands = async () => {
+  const response = await api.get('/brands');
+  return response.data;
+};
+
+export const createBrand = async (brandData) => {
+  const response = await api.post('/brands', brandData);
+  return response.data;
+};
